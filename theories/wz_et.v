10b7a9f94a5ddea9fdb02e48c51e15cd98eaae1f
jsCarleton <60323583+jsCarleton@users.noreply.github.com>
2026-01-21 00:33:44 -0500

    working on et

--- theories/wz_et.v
+++ theories/wz_et.v
@@ -1,6 +1,10 @@
 From Wasm Require Import datatypes pp.
 From Coq Require Import Floats.
+From Coq Require Import List.
 Require Import Strings.Byte Strings.String.
+From mathcomp Require Import all_ssreflect seq.
+Import ListNotations.
+
 
 (* variables *)
 Inductive var_type :=
@@ -49,9 +53,39 @@ Definition string_of_var (v: var): string :=
 Definition compare_vars (v1: var) (v2: var): comparison :=
   String.compare (string_of_var v1) (string_of_var v2).
 
+Definition valtype_of_var (param_types: result_type) (local_types: list value_type) (idx: nat): option value_type :=
+  if Nat.ltb idx (List.length param_types) then
+    List.nth_error param_types idx
+  else
+    List.nth_error local_types (idx - List.length param_types).
+
+Fixpoint vars_of_et' (tree: et): list var :=
+  match tree with
+  | Empty | Constant _  => [ ]
+  | Var v               => [v]
+  | ExprList el         => List.concat (List.map vars_of_et' el)
+  | Node _ _ args       => List.concat (List.map vars_of_et' args)
+  end.
+
+Definition lebv (v1: var) (v2: var): bool :=
+  leb (string_of_var v1) (string_of_var v2).
+
+Fixpoint dedup (vars: list var): list var :=
+  match vars with
+  | []        => []
+  | hd1::tl1  => match tl1 with
+                 | []     => [hd1]
+                 | hd2::_ => match compare_vars hd1 hd2 with
+                             | Eq  => dedup tl1
+                             | _   => hd1::(dedup tl1)
+                             end
+                 end
+  end.
+
+Definition vars_of_et (tree: et): list var :=
+  dedup (sort lebv (vars_of_et' tree)).
+
 (*
-val valtype_of_var:         Wm.resulttype list -> Wm.local_type list -> int -> Wm.valtype
-val vars_of_et:             et -> var list
 val v_in_vlist:             var -> var list -> bool
 val format_et:              et -> string
 val print_et:               et -> (string -> unit) -> unit
